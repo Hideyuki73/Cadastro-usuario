ffb073cd7cf07a959dcebd68052b2003f1a178bb
Hideyuki73 <rodrigokawakami123@gmail.com>
2025-09-29 22:17:01 -0400

    tela

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
-const app = express()
 const routes = require('./routes')
+const path = require('path')
+require('dotenv').config()
 
+const app = express()
 app.use(express.json())
-app.use(routes)
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/usuarios', routes)
 
 const PORT = 3000
 app.listen(PORT, () => console.log(`🚀 Servidor rodando em http://localhost:${PORT}`))
-
-module.exports = app // exporta para testes
